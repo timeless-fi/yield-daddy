a74d3ea2e6b3ae9697299dc9cb046f1d2d7bf686
ZeframLou <zeframlou@gmail.com>
2022-06-24 20:15:41 -0700

    build: add compound deploy script

---
+++ deploy/compound/mainnet.sh
@@ -0,0 +1,13 @@
+# load environment variables from .env
+source .env
+
+# set env variables
+export ADDRESSES_FILE=./deployments/mainnet.json
+export RPC_URL=$RPC_URL_MAINNET
+
+# load common utilities
+. $(dirname $0)/../common.sh
+
+# deploy contracts
+compound_factory_address=$(deploy CompoundERC4626Factory $COMPOUND_COMPTROLLER_MAINNET $COMPOUND_CETHER_MAINNET)
+echo "CompoundERC4626Factory=$compound_factory_address"
